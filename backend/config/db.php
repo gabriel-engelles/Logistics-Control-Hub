9e01a72e96311fa5b281d1538f55bb4bea8b1273
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-07-07 09:13:13 +0100

    establish initial database connection

--- backend/config/db.php
+++ backend/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=logistics_control_hub',
+    'username' => 'user_logistics',
+    'password' => '#3209@u/s(ER)?',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
