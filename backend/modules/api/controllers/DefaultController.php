e45eb1597818d33dfad28d09507ea024504b89ed
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-07-08 10:56:08 +0100

    Added CORS handling, JWT authentication, and role-based access control

--- backend/modules/api/controllers/DefaultController.php
+++ backend/modules/api/controllers/DefaultController.php
@@ -2,19 +2,54 @@
 
 namespace app\modules\api\controllers;
 
-use yii\web\Controller;
+use yii\filters\AccessControl;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\rest\ActiveController;
 
-/**
- * Default controller for the `api` module
- */
-class DefaultController extends Controller
-{
-    /**
-     * Renders the index view for the module
-     * @return string
-     */
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
+
+class DefaultController extends ActiveController{
+
+   public function behaviors()
+   {
+      $behaviors = parent::behaviors();
+
+      unset($behaviors['authenticator']);
+
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::class,
+        ];
+
+        $behaviors['authentication'] = [
+         'class' => CompositeAuth::class,
+         'authMethods' => [
+            HttpBearerAuth::class
+         ]
+      ];
+
+      $behaviors['access'] = [
+         'class' => AccessControl::class,
+         'rules' => [
+             [
+               'actions' => ['index', 'create', 'update', 'delete', 'view', 'set-role'],
+               'allow' => true,
+               'roles' => ['admin'],
+            ],
+            [
+               'actions' => [/* */],
+               'allow' => true,
+               'roles' => ['scheduler'],
+            ],
+            [
+               'actions' => [/* */],
+               'allow' => true,
+               'roles' => ['delivery_man'],
+            ],
+         ]
+      ];
+
+      return $behaviors;
+   }
+
+
 }
