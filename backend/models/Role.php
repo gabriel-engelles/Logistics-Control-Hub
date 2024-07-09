e960837d5be1ac08a602f6896e10115ca0e0a78b
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-07-09 23:55:41 +0100

    Include ROLE_SCHEDULER in the Role class

--- backend/models/Role.php
+++ backend/models/Role.php
@@ -7,11 +7,11 @@ use amnah\yii2\user\models\Role as AmnahRole;
 class Role extends AmnahRole
 {
     const ROLE_ADMIN = 1;
-    const ROLE_USER = 2;
+    const ROLE_SCHEDULER = 2;
     const ROLE_DELIVERY_MAN = 3;
 
     public static function getValidRoleIds()
     {
-        return [self::ROLE_ADMIN, self::ROLE_USER, self::ROLE_DELIVERY_MAN];
+        return [self::ROLE_ADMIN, self::ROLE_SCHEDULER, self::ROLE_DELIVERY_MAN];
     }
 }
