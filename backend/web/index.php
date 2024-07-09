546e8710a46a90b28e94a4466d93af80da90dc2a
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-07-09 22:25:49 +0100

    Refactored index.php

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,15 +1,22 @@
 <?php
 
+// Load Composer autoload
+require __DIR__ . '/../vendor/autoload.php';
+use Dotenv\Dotenv;
+
+$path = __DIR__ . '/../';
+
+if(!file_exists($path . '.env')){
+    die('.env file not found at ' . $path);
+}
+
+// Load environment variables from .env file
+$dotenv = Dotenv::createImmutable($path);
+$dotenv->load();
+
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-
-// Load Composer autoload
-require __DIR__ . '/../vendor/autoload.php';
-
-// Load environment variables from .env file
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
 
 // Yii Framework main file
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
